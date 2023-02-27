b32a7e2db5005a427c89e4348900d510883ffa7d
Kubinic <Osiptsov84@gmail.com>
2023-02-27 20:33:45 +0700

    Задачи 25 и 27

---
+++ task_25/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Напишите цикл, который принимает на вход два числа 
+(A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+Console.WriteLine("Введите число : ");
+double numb = double.Parse(Console.ReadLine()!);
+double pow = double.Parse(Console.ReadLine()!);
+Console.Write(Math.Pow(numb, pow));
---
+++ task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+Console.WriteLine("Введите число : ");
+int numb = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNumbers(numb));
+
+
+int SumNumbers( int numb){
+    int sum = 0;
+int digit = 0;
+for (int i = 1; numb > 0; i++){
+    digit = numb % 10;
+sum += digit;
+numb /= 10;
+}
+return sum;
+}
+
